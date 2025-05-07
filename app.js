d80b04a29c5e8f10557b82bf0ebbcaf78ddc9bfb
tmurr <34355872+tmurr@users.noreply.github.com>
2025-05-07 02:56:30 +0000

    create students.hbs file

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ app.get('/', async function (req, res) {
     }
 });
 
-app.get('/bsg-people', async function (req, res) {
+app.get('/Students', async function (req, res) {
     try {
         // Create and execute our queries
         // In query1, we use a JOIN clause to display the names of the homeworlds
-        const query1 = `SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
-            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
+        const query1 = `SELECT Students.studentId, Students.firstName, Students.lastName, \
+            Students.email, Students.major \
             LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;`;
         const query2 = 'SELECT * FROM bsg_planets;';
         const [people] = await db.query(query1);
@@ -53,7 +53,7 @@ app.get('/bsg-people', async function (req, res) {
 
         // Render the bsg-people.hbs file, and also send the renderer
         //  an object that contains our bsg_people and bsg_homeworld information
-        res.render('bsg-people', { people: people, homeworlds: homeworlds });
+        res.render('Students', { people: people, homeworlds: homeworlds });
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
